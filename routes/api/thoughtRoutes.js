5a58b5141b001e7d60eca7c2d01177a1e935bcde
Stacy <jlhclientsolutions@gmail.com>
2024-09-09 11:21:20 -0400

    finished routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,12 +1,28 @@
 const router = require('express').Router();
 const {
-    getSingleThought,
     getThoughts,
+    getSingleThought,
+    getThoughtReactions,
     createThought,
-} = require (//controller);
+    updateThought,
+    deleteThought,
+    addThoughtReaction,
+    removeThoughtReaction,
+} = require ('../../controllers/thoughtController');
 
-router.route('/').get(getThoughts).post(createThought);
+router.route('/')
+.get(getThoughts)
+.post(createThought);
 
-router.route('/:thoughtId').get(getSingleThought);
+router.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+router.route('/:thoughtId/reactions')
+.get(getThoughtReactions)
+.post(addThoughtReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeThoughtReaction);
 
 module.exports = router;
